e2db9a16a7bdd74a076904949e39e093236a8515
Eric Stock <eric.stock@K202Domain.CSN.com>
2025-05-26 11:10:20 -0230

    Added button example for useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Forecast from './Forecast.jsx'
 import weatherData from './weatherData.json'
+import { useState } from "react";
 
 
 function App() {
- 
+  let [count, setCount] = useState(0);
   return (
     <>
+      <button onClick={ () => {setCount(count+1); console.log(count)} }>Click Here</button>
+      <h2>Count is: {count}</h2>
       <h2>Eric's Weather for {weatherData.town}</h2>
       <Forecast data={weatherData.forecast}/>
+      
     </>
   )
 }
